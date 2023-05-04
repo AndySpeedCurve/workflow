ddf5aa9d261bac7538e422eabe8c6f3b9c83ea44
Andy Biggs <andy@speedcurve.com>
2023-05-04 13:38:02 +1200

    Causing test to fail.

--- lib/animal.ts
+++ lib/animal.ts
@@ -4,7 +4,7 @@ export function getAnimalDescription(animalName: string): string {
             return "A gregarious burrowing plant-eating mammal, with long ears, long hind legs, and a short tail.";
             break;
         case "kiwi":
-            return "A flightless New Zealand bird with hairlike feathers, having a long downcurved bill with sensitive nostrils at the tip.";
+            return "A flightless New Zealand bird with hairlike feathers, having a long downcurved bill with sensitive nostrils at the tip. It's also a piece of fruit!";
             break;
         case "cat":
             return "A small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws. It is widely kept as a pet or for catching mice, and many breeds have been developed.";
